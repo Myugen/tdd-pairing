0c5af1a95505f3684335f97a98ebde94ca283b8b
Miguel Cabrera <miguelcabsan@gmail.com>
2020-11-21 19:01:36 +0000

    feat(http): Add v1 endpoint

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
+type rootHandler struct{}
+
 func main() {
 	e := setupServer()
+
+	root := e.Group("/api")
+	h := &rootHandler{}
+	v1 := root.Group("/v1")
+	{
+		v1.GET("", h.Ping)
+	}
 	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+func (h *rootHandler) Ping(c echo.Context) error {
+	return c.String(http.StatusOK, "Ok!")
 }
 
 func HelloWorld() string {
--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/labstack/echo/v4"
 
 	. "github.com/franela/goblin"
 )
@@ -13,10 +17,33 @@ func Test(t *testing.T) {
 			message := HelloWorld()
 			g.Assert(message).Equal("Hello, world!")
 		})
-
 		g.It("Should setup a new server", func() {
 			e := setupServer()
 			g.Assert(e).IsNotNil()
+		})
+	})
+}
+
+func TestHttpServer(t *testing.T) {
+	g := Goblin(t)
+	var e *echo.Echo
+	g.Describe("Http server", func() {
+
+		g.BeforeEach(func() {
+			e = setupServer()
+		})
+
+		g.Describe("Request to v1 endpoint", func() {
+			g.It("Should response with 200 Status OK", func() {
+				req := httptest.NewRequest(http.MethodGet, "/api/v1", nil)
+				rec := httptest.NewRecorder()
+				c := e.NewContext(req, rec)
+				h := &rootHandler{}
+
+				g.Assert(h.Ping(c)).IsNil()
+				g.Assert(rec.Code).Equal(http.StatusOK)
+				g.Assert(rec.Body.String()).Equal("Ok!")
+			})
 		})
 	})
 }
